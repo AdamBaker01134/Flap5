bcf5bb5f1f293254b8447269ae2d47cf39d43f86
Adam Baker <adb888@usask.ca>
2023-05-28 21:12:14 -0600

    Added p5 project boilerplate: html page + canvas.js with setup and draw functions that draw a canvas on the page.

---
+++ canvas.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const DISPLAY_WIDTH = 1280;
+const DISPLAY_HEIGHT = 1024;
+
+let model;
+let view;
+let controller;
+
+function setup() {
+    createCanvas(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+}
+
+function draw() {}
